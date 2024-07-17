e4d5105e65eef2019dccc9a20951c7a9037987c6
PrashanthPremchand <prashanthpremchand@gmail.com>
2024-07-17 19:42:25 +0530

    JavaScript fundamentals2

---
+++ script.js
@@ -0,0 +1,15 @@
+// function fun(){
+//     let l;
+//     if(true){
+//         l = 10;
+//     }
+//     console.log(l);
+// }
+// console.log("Hello");
+let hi = 100;
+if(hi > 50){
+    console.log("I am happy");
+} 
+else {
+    console.log("I am sad");
+}
